cb99b2a39add27dd81a4fc48f2257a071f00746d
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2025-03-31 19:16:35 +0200

    Push all files

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main(void)
   
   GPIO_InitStruct.Pin = LED_PIN;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct); 
 
